b18e43ecbe0c821d034e02c1de5ed7c6d9dda142
Samuel <samueldelcastillo1710@gmail.com>
2023-09-13 19:23:37 -0600

    main- clase miercoles 13 sep

--- PPV2-N2/Assets/scripts/UI/UI_box.cs
+++ PPV2-N2/Assets/scripts/UI/UI_box.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class UI_box : MonoBehaviour
 {
+    private UI_box_BG BoxBG;
+    private UI_box_Header BoxHeader;
+    private UI_box_header_title BoxHeaderTitle;
+    private UI_box_container_Title BoxContent;
 
-    public UI_box_header_title BoxHeaderTitle;
     public string title = "";
     public string content = "";
-
+   
+    public bool HasCloseBtn = true;
     void Start()
     {
+        BoxBG = transform.GetChild(0).GetComponent<UI_box_BG>();
+        BoxHeader = transform.GetChild(0).transform.GetChild(0).GetComponent<UI_box_Header>();
+        BoxHeaderTitle = transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).GetComponent<UI_box_header_title>();
+        BoxContent = transform.GetChild(0).transform.GetChild(1).transform.GetChild(0).GetComponent<UI_box_container_Title>();
         BoxHeaderTitle.Title.text = title;
+        BoxContent.content.text = content;
+        
+
+        transform.GetChild(0).transform.GetChild(0).transform.GetChild(1).gameObject.SetActive(HasCloseBtn);
     }
 
     // Update is called once per frame
--- PPV2-N2/Assets/scripts/UI/UI_box_container_Title.cs
+++ PPV2-N2/Assets/scripts/UI/UI_box_container_Title.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UI_box_container_Title : MonoBehaviour
 {
+    public TMP_Text content;
+    public TMP_Text content2;
+    public float masa;
+    public int valor;
     // Start is called before the first frame update
     void Start()
     {
+        content = GetComponent<TMP_Text>();
         
     }
 
