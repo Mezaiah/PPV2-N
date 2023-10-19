20b9b27df2e2037d6fcdc6ba7edee758caf6d37b
Samuel <samueldelcastillo1710@gmail.com>
2023-10-18 20:18:10 -0600

    main - 18 oct clase

--- PPV2-N2/Assets/scripts/2°Parcial/FileSystem.cs
+++ PPV2-N2/Assets/scripts/2°Parcial/FileSystem.cs
@@ -10,25 +10,42 @@ public class FileSystem : MonoBehaviour
     public GameObject obj2;
     string positionS;
     bool IsSaving = false;
-    void CreateFile()
+    public Player_data p;
+    void CreateFile(string _FileName, string _extension, string _data)
     {
         //1) path
-        string FileName = "Position";
-        string extension = ".txt";
-        string path = Application.dataPath + "/Resources/" + FileName + extension;
+        //string FileName = "Example";
+        //string extension = ".txt";
+        string path = Application.dataPath + "/Resources/" + _FileName + _extension;
+
 
         //2)crear archivo si no existe ya uno con el mismo nombre
         if (!File.Exists(path))
         {
         //3)escribir dentro del archivo
-            File.WriteAllText(path, "");
+            //File.WriteAllText(path, "");
         }
         //4)Almacenar el contenido
         //string data = " Login Date: " + System.DateTime.Now + "\n" + positionS;
-        string data = positionS;
+        //string data = positionS;
         ////5)agregart la info al archivo
-        File.AppendAllText(path, data);
+        File.AppendAllText(path, _data);
 
+
+    }
+
+    public void SaveToJSON(string _FIleName, object _data)
+    {
+        string JSONData = JsonUtility.ToJson(_data);
+        if(JSONData.Length != 0)
+        {
+            Debug.Log("jSON string" + JSONData);
+            CreateFile(_FIleName,".json", JSONData);
+        }
+        else
+        {
+            Debug.LogWarning("Tas_Wey");
+        }
 
     }
     string ReadFile(string _FileName, string _Extension)
@@ -83,9 +100,30 @@ public class FileSystem : MonoBehaviour
         positionS = position.ToString();
         Debug.Log(positionS);
     }
+
+    Player_data LoadFromJSON(string _FileName)
+    {
+        Player_data data = new Player_data();
+       
+        string JSONData = ReadFile(_FileName, ".json");
+        if (JSONData.Length != 0)
+        {
+            Debug.Log("Data: " + JSONData);
+            JsonUtility.FromJsonOverwrite(JSONData, data);
+        }
+        else
+        {
+            Debug.LogWarning("Tas_Wey");
+        }
+        return data;
+    }
     // Start is called before the first frame update
     void Start()
     {
+
+         //= new Player_data("Sam", "Penudo", 565656);
+        p = (Player_data)LoadFromJSON("Sam");
+        //SaveToJSON(p.Name, p);
         string data = ReadFile("Position", ".txt");
         Debug.Log("Información de archivo: \n " + data);
         Vector3 position = ReadPositionFromText("Position", ".txt");
